649437989b04f50c71f745011bff2de299c276eb
Lewainne Vidal <lewainne.vidal@adtelligent.net>
2022-04-14 16:09:07 -0500

    Completed the main script to get the daily youtube subscribers - next is to connect to mongoose

---
+++ server.js
@@ -0,0 +1,28 @@
+const ytch = require ('yt-channel-info');
+
+/**
+ * To get the youtube channel stats for Grace Foods
+ * @param {*} channelId 
+ */
+async function getChannelDetails (channelId) {
+    const payload = {
+        channelId: channelId,
+        channelIdType: 0,
+        httpsAgent: ''
+    };//payload for the api
+    ytch.getChannelInfo(payload).then((res) => {
+        let channelDetails = {
+            author: res.author,
+            channelId: res.authorId,
+            description: res.description,            
+            subscribers: res.subscriberCount
+        };
+        console.log(channelDetails);
+        return channelDetails;
+    }).catch((err) => {
+        console.log("There was an error");
+        console.error(err);
+    });
+}
+
+getChannelDetails('UC7_LPOqjqw5eWezUpe7mLoA');
